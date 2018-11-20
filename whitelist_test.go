69af92bffb5a9c5a59fb0cda096ff44f2af2438e
Spencer Ellinor <zpencerq@users.noreply.github.com>
2018-11-20 13:47:23 -0800

    Add failing test that shows WhitelistManager panics with nil URL Request

---
+++ whitelist_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/zpencerq/goproxy"
+)
+
+func AssertDidNotPanic(t *testing.T, name string, f func()) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("%s did panic!", name)
+		}
+	}()
+
+	f()
+}
+
+func DummyReq(t *testing.T) *http.Request {
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.RemoteAddr = "127.0.0.1:57862"
+
+	return req
+}
+
+func HandlerInput(r *http.Request) (*http.Request, *goproxy.ProxyCtx) {
+	return r, &goproxy.ProxyCtx{Req: r}
+}
+
+func TestReqHandler(t *testing.T) {
+	source := NewMemoryWhitelistLoader(
+		[]Entry{NewEntry("https://google.com")},
+	)
+	wm, _ := NewWhitelistManager(source)
+
+	r := DummyReq(t)
+	r.URL = nil
+
+	run := func() { wm.ReqHandler()(HandlerInput(r)) }
+
+	AssertDidNotPanic(t, "ReqHandler with nil URL", run)
+}
